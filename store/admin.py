90cd0ae5dd2d29fca7b937e0c7932237b7a0a82e
MagFou <gfggjxudxudusbs@gmail.com>
2025-04-04 09:08:58 +0500

    общий дизайн

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,14 @@
+# store/admin.py
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'discount_price', 'in_stock')
     list_filter = ('in_stock',)
+    search_fields = ('name',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
     search_fields = ('name',)
--- store/models.py
+++ store/models.py
@@ -1,11 +1,26 @@
+# store/models.py
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+# store/models.py
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     discount_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
     in_stock = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')  # Убираем null=True, blank=True
 
     def __str__(self):
         return self.name
+
+    @property
+    def discount_percent(self):
+        if self.discount_price and self.price:
+            return int(((self.price - self.discount_price) / self.price) * 100)
+        return None
