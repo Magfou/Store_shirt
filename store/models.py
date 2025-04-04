1d95d4796be1562970d334d8393ee179cacf65c7
MagFou <gfggjxudxudusbs@gmail.com>
2025-04-04 12:14:34 +0500

    пагинация

--- store/models.py
+++ store/models.py
@@ -1,26 +1,28 @@
-# store/models.py
 from django.db import models
 
+# Модель категории
 class Category(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100, verbose_name="Название категории")
 
     def __str__(self):
         return self.name
 
-# store/models.py
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+# Модель товара
 class Product(models.Model):
-    name = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    discount_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
-    in_stock = models.BooleanField(default=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')  # Убираем null=True, blank=True
+    name = models.CharField(max_length=200, verbose_name="Название")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Категория")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    discount_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="Цена со скидкой")
+    image = models.ImageField(upload_to='products/', null=True, blank=True, verbose_name="Изображение")
+    in_stock = models.BooleanField(default=True, verbose_name="В наличии")
 
     def __str__(self):
         return self.name
 
-    @property
-    def discount_percent(self):
-        if self.discount_price and self.price:
-            return int(((self.price - self.discount_price) / self.price) * 100)
-        return None
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
