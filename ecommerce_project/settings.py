6be0f82518f8b5f3be40fc2c7856b0d98e136132
MagFou <gfggjxudxudusbs@gmail.com>
2025-04-08 00:07:03 +0500

    lastt

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@your-secret-key-here'  # Замените на с
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'store-shirt']
 
 # Application definition
 
